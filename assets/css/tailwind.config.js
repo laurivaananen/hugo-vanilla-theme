6763bd854dbd0194bd4da27efa471c6a125e4bb2
Lauri Väänänen <lauri.e.vaananen@helsinki.fi>
2020-08-13 15:48:41 +0200

    update theme

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
+      boxShadow: {
+        purple: "2px 2px 8px 0px rgba(255, 51, 109, 0.7)"
+      }
     }
   },
   variants: {
